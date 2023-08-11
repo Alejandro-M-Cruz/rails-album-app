4b3091fc5ccffc26a6da185533a1d198c45afe10
unknown <alejandromatiascc@gmail.com>
2023-08-11 01:06:18 +0100

    navbar links displayed appropriately depending on current page

---
+++ app/helpers/navbar_helper.rb
@@ -0,0 +1,21 @@
+module NavbarHelper
+
+  def navbar_links
+    navbar_links = [
+      { name: 'Home', path: root_path },
+      { name: 'New image', path: new_image_path }
+    ]
+    navbar_links.map { |link| link_html(link) }.join.html_safe
+  end
+
+  private
+
+  def link_html(link)
+    "<li>#{link_to link[:name], link[:path], class: link_class(link[:path])}</li>"
+  end
+
+  def link_class(link_path)
+    current_page?(link_path) ? 'nav-link px-2 text-secondary' : 'nav-link px-2 text-white'
+  end
+
+end
