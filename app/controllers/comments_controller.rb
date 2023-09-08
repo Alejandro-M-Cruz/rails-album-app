c3355eb85e4a96861b7cc8783f896711e3d74acc
unknown <alejandromatiascc@gmail.com>
2023-09-08 10:02:05 +0100

    comment updates only refresh comments section and alerts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,8 @@ class CommentsController < ApplicationController
   def create
     @comment = @commentable.comments.new(**comment_params, user: current_user)
     if @comment.save
-      redirect_to @commentable, notice: 'Comment successfully created'
+      # redirect_to @commentable, notice: 'Comment successfully created'
+      refresh_comments_section('Comment successfully created')
     else
       refresh_comment_form
     end
@@ -20,7 +21,8 @@ class CommentsController < ApplicationController
   # PATCH/PUT /comments/1 or /comments/1.json
   def update
     if @comment.update(comment_params)
-      redirect_to @commentable, notice: 'Comment successfully updated'
+      # redirect_to @commentable, notice: 'Comment successfully updated'
+      refresh_comments_section('Comment successfully updated')
     else
       refresh_comment_form
     end
@@ -29,7 +31,8 @@ class CommentsController < ApplicationController
   # DELETE /comments/1 or /comments/1.json
   def destroy
     if @comment.destroy
-      redirect_to @commentable, notice: 'Comment successfully deleted'
+      # redirect_to @commentable, notice: 'Comment successfully deleted'
+      refresh_comments_section('Comment successfully deleted')
     else
       refresh_comment_form
     end
@@ -76,6 +79,21 @@ class CommentsController < ApplicationController
             partial: 'comments/form',
             locals: { comment: @comment, commentable: @commentable }
           )
+        end
+      end
+    end
+
+    def refresh_comments_section(notice_message)
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace(
+              dom_id(@commentable, :comments),
+              partial: 'comments/comments',
+              locals: { commentable: @commentable }
+            ),
+            helpers.show_flash(:notice, notice_message)
+          ]
         end
       end
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def show_flash(type, message)
+    flash[type] = message
+    turbo_stream.append("flash-#{type.to_s}-container", partial: "shared/flash/#{type.to_s}")
+  end
 end
